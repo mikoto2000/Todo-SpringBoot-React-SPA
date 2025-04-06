4a65ae1eac6630a4adf1d2d1f8e3ae65fa7a4029
mikoto2000 <mikoto2000@gmail.com>
2025-04-06 05:48:57 +0000

    フロントエンドにログイン処理を追加。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,50 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Todo } from './model/Todo';
 import * as TodoService from './service/TodoService';
+import { useAuth } from 'react-oidc-context';
 
 function App() {
+
+  const auth = useAuth();
 
   const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
-    const newTodos = TodoService.getTodo();
-    setTodos(newTodos);
+    if (auth.isAuthenticated) {
+      const newTodos = TodoService.getTodo();
+      setTodos(newTodos);
+    }
   });
 
-  return (
-    <>
-      <h1>Todo SPA Application</h1>
-      <h2>Add Task</h2>
-      <div>
-        <input type="text"></input>
-        <input type="submit" value="Send"></input>
-      </div>
-      <h2>Todos</h2>
-      <div>
-      <ul>
-        {
-          todos.map(e => <li key="{e.id}">{e.id} : {e.title}</li>)
-        }
-      </ul>
-      </div>
-    </>
-  )
+  if (auth.isAuthenticated) {
+
+    return (
+      <>
+        <h1>Todo SPA Application</h1>
+        <h2>Add Task</h2>
+        <div>
+          <input type="text"></input>
+          <input type="submit" value="Send"></input>
+        </div>
+        <h2>Todos</h2>
+        <div>
+          <ul>
+            {
+              todos.map(e => <li key="{e.id}">{e.id} : {e.title}</li>)
+            }
+          </ul>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Todo SPA Application</h1>
+        <h2>Please login</h2>
+        <button onClick={() => auth.signinRedirect()}>Login</button>
+      </>
+    )
+  }
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from 'react-oidc-context';
+
+const oidcConfig = {
+  authority: 'http://keycloak:8080/realms/myrealm',
+  client_id: 'client-id',
+  client_secret: 'client-secret',
+  redirect_uri: 'http://localhost:5173/',
+  post_logout_redirect_uri: 'http://localhost:5173/',
+  scope: 'openid profile',
+  response_type: 'code',
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
+
