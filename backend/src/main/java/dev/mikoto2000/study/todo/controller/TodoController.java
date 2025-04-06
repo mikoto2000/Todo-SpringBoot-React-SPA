110cc5b19db67fcf114e9e1bd7ac5d08bb59112f
mikoto2000 <mikoto2000@gmail.com>
2025-04-06 09:12:13 +0000

    ページネーション処理の土台を実装。

--- backend/src/main/java/dev/mikoto2000/study/todo/controller/TodoController.java
+++ backend/src/main/java/dev/mikoto2000/study/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.mikoto2000.study.todo.dto.Todo;
@@ -24,8 +25,14 @@ public class TodoController {
 
   @GetMapping("/todos")
   public List<Todo> getTodos(
-      @AuthenticationPrincipal Jwt jwt) {
-    return todoMapper.findAllByEmail(jwt.getClaimAsString("email"));
+      @AuthenticationPrincipal Jwt jwt,
+      @RequestParam(defaultValue = "20") int size,
+      @RequestParam(defaultValue = "0") int page) {
+
+    int offset = size * page;
+    int limit = size;
+
+    return todoMapper.findAllByEmail(jwt.getClaimAsString("email"), offset, limit);
   }
 
   @PostMapping("/todos")
--- backend/src/main/java/dev/mikoto2000/study/todo/repository/TodoMapper.java
+++ backend/src/main/java/dev/mikoto2000/study/todo/repository/TodoMapper.java
@@ -14,8 +14,8 @@ import dev.mikoto2000.study.todo.dto.Todo;
  */
 @Mapper
 public interface TodoMapper {
-  @Select("SELECT id,email,title,done FROM todo WHERE email = #{email}")
-  List<Todo> findAllByEmail(String email);
+  @Select("SELECT id,email,title,done FROM todo WHERE email = #{email} LIMIT #{limit} OFFSET #{offset}")
+  List<Todo> findAllByEmail(String email, int offset, int limit);
 
   @Insert("INSERT INTO todo (email,title,done) VALUES (#{email},#{title},#{done})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
