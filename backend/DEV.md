7cdb7c96d1c6a254e92fbbd15cd644445bc59155
mikoto2000 <mikoto2000@gmail.com>
2025-04-06 04:51:24 +0000

    Added security configurations.

--- backend/DEV.md
+++ backend/DEV.md
@@ -1,5 +1,5 @@
 # Project
 
-[Spring Initializr](https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.4&packaging=jar&jvmVersion=24&groupId=dev.mikoto2000.study&artifactId=todo&name=todo&description=TODO%20Demo%20project%20for%20Spring%20Boot&packageName=dev.mikoto2000.study.todo&dependencies=devtools,lombok,web,security,oauth2-resource-server,mybatis,postgresql,validation)
+[Spring Initializr](https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.4&packaging=jar&jvmVersion=21&groupId=dev.mikoto2000.study&artifactId=todo&name=todo&description=TODO%20Demo%20project%20for%20Spring%20Boot&packageName=dev.mikoto2000.study.todo&dependencies=devtools,lombok,web,security,oauth2-resource-server,mybatis,postgresql,validation)
 
 
---
+++ backend/src/main/java/dev/mikoto2000/study/todo/configuration/SecurityConfiguration.java
@@ -0,0 +1,85 @@
+package dev.mikoto2000.study.todo.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  /**
+   * 認可のための SecurityFilterChain を設定
+   */
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            // 全てのアクセスに対して認証を要求する
+            .anyRequest().authenticated())
+        // セッションをステートレスモードにする
+        // ステートレスな Web API として実装するならステートはいらない
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // SPA の CSRF 対策として、クッキーに CSRF トークンを保存し、 JS から読めるようにしておく
+        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+
+    return http.build();
+  }
+
+  /**
+   * CORS の設定
+   */
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    var configuration = new CorsConfiguration();
+
+    // Access-Control-Allow-Origin の設定
+
+    // setAllowedOriginPatterns はワイルドカードを使えないので注意
+    // configuration.setAllowedOrigins(List.of("http://host.docker.internal:3000"));
+
+    // ワイルドカードを使いたい場合は setAllowedOriginPatterns を使用
+    configuration.setAllowedOriginPatterns(List.of("*"));
+
+    // Access-Control-Allow-Methods の設定
+    configuration.setAllowedMethods(List.of("*"));
+
+    // Access-Control-Allow-Headers の設定
+    configuration.setAllowedHeaders(List.of("*"));
+
+    // Access-Control-Allow-Credentials の設定
+    configuration.setAllowCredentials(true);
+
+    var source = new UrlBasedCorsConfigurationSource();
+
+    // COSR設定を行う範囲のパスを指定する。この例では全てのパスに対して設定が有効になる
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+  }
+
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+    jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+    return jwtAuthenticationConverter;
+  }
+}
